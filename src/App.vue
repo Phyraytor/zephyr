e87e2e3ca187ed2f9ea0b31bcd92b18bf7aed6a6
Phyraytor <phyraytor@gmail.com>
2020-09-06 01:51:41 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      
+    </header>
+    <main>
+      <popover placement = "right" innerWidth = "200px">
+        <tbody slot = "content"><tr class="el-table__row"><td rowspan="1" colspan="1" class="el-table_1_column_1  "><div class="cell">2016-05-02</div></td><td rowspan="1" colspan="1" class="el-table_1_column_2  "><div class="cell">Jack</div></td><td rowspan="1" colspan="1" class="el-table_1_column_3  "><div class="cell">New York City</div></td></tr><tr class="el-table__row"><td rowspan="1" colspan="1" class="el-table_1_column_1  "><div class="cell">2016-05-04</div></td><td rowspan="1" colspan="1" class="el-table_1_column_2  "><div class="cell">Jack</div></td><td rowspan="1" colspan="1" class="el-table_1_column_3  "><div class="cell">New York City</div></td></tr><tr class="el-table__row"><td rowspan="1" colspan="1" class="el-table_1_column_1  "><div class="cell">2016-05-01</div></td><td rowspan="1" colspan="1" class="el-table_1_column_2  "><div class="cell">Jack</div></td><td rowspan="1" colspan="1" class="el-table_1_column_3  "><div class="cell">New York City</div></td></tr><tr class="el-table__row"><td rowspan="1" colspan="1" class="el-table_1_column_1  "><div class="cell">2016-05-03</div></td><td rowspan="1" colspan="1" class="el-table_1_column_2  "><div class="cell">Jack</div></td><td rowspan="1" colspan="1" class="el-table_1_column_3  "><div class="cell">New York City</div></td></tr><!----></tbody>
+        <button slot="button">Нажми</button>
+      </popover>
+      <popover placement = "bottom">
+        <div slot = "content">
+          <input type="text"><button>Submit</button>
+        </div>
+        <button slot="button">Нажми</button>
+      </popover> 
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Popover from './components/Popover.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Popover
   }
 }
 </script>
@@ -19,10 +32,16 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+header {
+  width: 100%;
+  background-color: #111;
+  height: 70px;
+}
+main {
+  width: 1040px;
+  margin: 20px auto;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/Popover.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="popover">
+    <div v-show = "showContent" class="popover__content" ref = "popoverContent" v-bind:style="style">
+      <slot name = "content"></slot>
+    </div>
+    <div class="popover__button" ref = "popoverButton" @click = "setPlacement()">
+      <slot name="button"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Popover',
+  props: {
+    placement: {
+      type: String,
+      validator: function (value) {
+        return ['top', 'right', 'bottom', 'left'].indexOf(value) !== -1;
+      }
+     },
+    offset: {
+      type: Number,
+      default: 10,
+    },
+    innerWidth: {
+      type: Number,
+      default: 150,
+    },
+  },
+  data: function() {
+    return {
+      showContent: true,
+      style: {
+        top: 0,
+        left: 0,
+        width: this.innerWidth,
+      },
+      heightContent: 0,
+      widthContent: 0,
+    }
+  },
+  methods: {
+    findClassName(el, cls) {
+      while ( (el = el.parentElement) && !el.classList.contains(cls) );
+      return el;
+    },
+    setPlacement() {
+      const heightButton = this.$refs.popoverButton.clientHeight,
+            widthButton = this.$refs.popoverButton.clientWidth;
+      switch(this.placement) {
+        case 'top':
+          this.$set(this.style, 'top', -(this.offset + this.heightContent) + "px");
+          this.$set(this.style, 'left', (widthButton / 2 - this.widthContent / 2) + "px");   
+          break;
+        case 'right':
+          this.$set(this.style, 'left', (+widthButton + this.offset) + "px");
+          this.$set(this.style, 'top', (heightButton / 2 - this.heightContent / 2) + "px");
+          break;
+        case 'bottom':
+          this.$set(this.style, 'top', this.offset + heightButton + "px");
+          this.$set(this.style, 'left', (widthButton / 2 - this.widthContent / 2) + "px");
+          break;
+        case 'left':
+          this.$set(this.style, 'left', -(this.offset + this.widthContent) + "px");
+          this.$set(this.style, 'top', (-this.heightContent / 2 + heightButton / 2) + "px");
+          break;
+      }
+    }
+  },
+
+  mounted() {
+    this.heightContent = this.$refs.popoverContent.clientHeight;
+    this.widthContent = this.$refs.popoverContent.clientWidth;
+    this.showContent = false;
+  },
+  created() {
+    const _this = this;
+    document.addEventListener('click', function (e) {
+      const isButton = _this.findClassName(e.target, 'popover__button');
+      const isContent = _this.findClassName(e.target, 'popover__content');
+      const inPopover =  _this.findClassName(e.target, 'popover') === _this.$el ;
+
+      _this.showContent = (isContent && inPopover 
+        || !_this.showContent && isButton  && inPopover);
+
+    });
+  },
+}
+</script>
+
+<style scoped>
+  .popover {
+    position: relative;
+  } 
+  .popover__button {
+    display: inline-block;
+  }
+  .popover__content {
+    position: absolute;
+    background: #fff;
+    min-width: 150px;
+    border-radius: 4px;
+    border: 1px solid #ebeef5;
+    padding: 12px;
+    z-index: 2000;
+    color: #606266;
+    line-height: 1.4;
+    text-align: justify;
+    font-size: 14px;
+    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+    overflow-x: auto;
+  }
+</style>
